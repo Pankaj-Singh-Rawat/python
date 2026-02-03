46315fea537297f3d1e156d90b93c86e482872da
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-02-03 10:20:16 +0530

    print text and numbers

---
+++ print_text_and_num.py
@@ -0,0 +1,13 @@
+# texts are supposed to be inside "here" or 'here' 
+print("hello world ")
+
+
+# to print multiple things on the same line we can use end
+print("hello", end = " ")
+print("world!")
+
+# to print numbers we do not need to use any quotes we can just write
+print(5000) 
+
+# we can also mix text and numbers like this 
+print("my name is Pankaj, and I was born in", 2004, "glad to meet you!" )
