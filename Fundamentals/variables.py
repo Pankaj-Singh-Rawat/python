3a3b82711cedc1ea1d62c80b745f5962141b0a76
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-02-03 12:11:28 +0530

    variables and file changes

---
+++ Fundamentals/variables.py
@@ -0,0 +1,9 @@
+# A variable is created the moment you assign a value to it. 
+# no need to write data type
+x = 5 # int data type
+y = "pankaj" # String data type
+print(x)
+print(y)
+
+x = 23.34 # float data type
+print(x)
