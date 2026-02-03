540c2525e4ecbd7f63804606cf3af045ad2f7c3b
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-02-03 12:28:23 +0530

    Assigning multiple values

--- Fundamentals/variables.py
+++ Fundamentals/variables.py
@@ -7,3 +7,28 @@ print(y)
 
 x = 23.34 # float data type
 print(x)
+
+print()
+
+# assigning multiple variables
+x,y,z = "pankaj", "singh", "rawat"
+print(x)
+print(y)
+print(z)
+
+print()
+
+# one value to multiple variables
+x = y = z = "Demon"
+print(x)
+print(y)
+print(z)
+
+print()
+
+# unpacking a collection: if you have a pack of tuple, list etc you can put it in variables
+fruits = ["pankaj", "singh", "rawat"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
