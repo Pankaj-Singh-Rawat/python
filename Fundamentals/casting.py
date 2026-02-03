8e3c83f0bd59eaa0188a0bb4ff5f0b2507999db7
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-02-03 12:19:20 +0530

    type casting

---
+++ Fundamentals/casting.py
@@ -0,0 +1,22 @@
+# to convert into another data type we can just write the name of the data type along with the data we need to pass
+
+x = 5 # int data type 
+
+# lets say I want to convert it to String then, 
+y = str(x) # now value of c is of type string
+print(y)
+
+# to check the data type of a variable we can use type()
+
+print(type(x)) # must print int
+print(type(y)) # must print str
+
+# similarly
+y = float(x)
+print(type(y))
+print(y)
+
+
+
+
+
