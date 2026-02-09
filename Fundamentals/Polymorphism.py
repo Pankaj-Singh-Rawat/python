32e81605aecd1f5d127cc8f5b8d53c0d80cb4388
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-02-09 09:44:49 +0530

    Polymorphism

---
+++ Fundamentals/Polymorphism.py
@@ -0,0 +1,20 @@
+class A:
+    def __init__(self,name):
+        self.name = name
+
+class B:
+    def __init__(self,name):
+        self.name = name
+
+class C:
+    def __init__(self,name):
+        self.name = name
+
+
+obj1 = A("pankaj")
+print(obj1.name)
+obj2 = A("singh")
+print(obj2.name)
+obj3 = A("rawat")
+print(obj3.name)
+
